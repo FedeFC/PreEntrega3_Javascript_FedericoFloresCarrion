83441177f55e83ad6b48d6acb05fd654f6c3d8fa
FedericoFc <f.florescarrion@gmail.com>
2023-01-10 23:10:55 -0300

    fetch agregado

--- Final/script.js
+++ Final/script.js
@@ -1,175 +1,180 @@
-let productos = [
-  {
-    id: "arnes-01",
-    titulo: "Arnes 01",
-    imagen: "./productos/arnes01.jpg",
-    categoria: {
-      nombre: "Arneses",
-      id: "arneses",
-    },
-    precio: 1500,
-  },
-  {
-    id: "arnes-02",
-    titulo: "Arnes 02",
-    imagen: "./productos/arnes02.jpg",
-    categoria: {
-      nombre: "Arneses",
-      id: "arneses",
-    },
-    precio: 1700,
-  },
-  {
-    id: "arnes-03",
-    titulo: "Arnes 03",
-    imagen: "./productos/arnes03.jpg",
-    categoria: {
-      nombre: "Arneses",
-      id: "arneses",
-    },
-    precio: 1200,
-  },
-  {
-    id: "arnes-04",
-    titulo: "Arnes 04",
-    imagen: "./productos/arnes04.jpg",
-    categoria: {
-      nombre: "Arneses",
-      id: "arneses",
-    },
-    precio: 1800,
-  },
-  {
-    id: "casco-01",
-    titulo: "Casco 01",
-    imagen: "./productos/casco01.jpg",
-    categoria: {
-      nombre: "Cascos",
-      id: "cascos",
-    },
-    precio: 10200,
-  },
-  {
-    id: "casco-02",
-    titulo: "Casco 02",
-    imagen: "./productos/casco02.jpg",
-    categoria: {
-      nombre: "Cascos",
-      id: "cascos",
-    },
-    precio: 10900,
-  },
-  {
-    id: "casco-03",
-    titulo: "Casco 03",
-    imagen: "./productos/casco03.jpg",
-    categoria: {
-      nombre: "Cascos",
-      id: "cascos",
-    },
-    precio: 15200,
-  },
-  {
-    id: "casco-04",
-    titulo: "Casco 04",
-    imagen: "./productos/casco04.jpg",
-    categoria: {
-      nombre: "Cascos",
-      id: "cascos",
-    },
-    precio: 17800,
-  },
-  {
-    id: "casco-05",
-    titulo: "Casco 05",
-    imagen: "./productos/casco05.jpg",
-    categoria: {
-      nombre: "Cascos",
-      id: "cascos",
-    },
-    precio: 19999,
-  },
-  {
-    id: "cuerda-01",
-    titulo: "Cuerda 01",
-    imagen: "./productos/cuerda01.jpeg",
-    categoria: {
-      nombre: "Cuerdas",
-      id: "cuerdas",
-    },
-    precio: 70200,
-  },
-  {
-    id: "cuerda-02",
-    titulo: "Cuerda 02",
-    imagen: "./productos/cuerda02.jpg",
-    categoria: {
-      nombre: "Cuerdas",
-      id: "cuerdas",
-    },
-    precio: 69200,
-  },
-  {
-    id: "cuerda-03",
-    titulo: "Cuerda 03",
-    imagen: "./productos/cuerda03.jpg",
-    categoria: {
-      nombre: "Cuerdas",
-      id: "cuerdas",
-    },
-    precio: 80000,
-  },
-  {
-    id: "mosqueton-01",
-    titulo: "Mosqueton 01",
-    imagen: "./productos/mosqueton01.jpg",
-    categoria: {
-      nombre: "Mosquetones",
-      id: "mosquetones",
-    },
-    precio: 3500,
-  },
-  {
-    id: "mosqueton-02",
-    titulo: "Mosqueton 02",
-    imagen: "./productos/mosqueton02.jpg",
-    categoria: {
-      nombre: "Mosquetones",
-      id: "mosquetones",
-    },
-    precio: 4500,
-  },
-  {
-    id: "mosqueton-03",
-    titulo: "Mosqueton 03",
-    imagen: "./productos/mosqueton03.jpg",
-    categoria: {
-      nombre: "Mosquetones",
-      id: "mosquetones",
-    },
-    precio: 4300,
-  },
-  {
-    id: "mosqueton-04",
-    titulo: "Mosqueton 04",
-    imagen: "./productos/mosqueton04.jpg",
-    categoria: {
-      nombre: "Mosquetones",
-      id: "mosquetones",
-    },
-    precio: 5150,
-  },
-  {
-    id: "mosqueton-05",
-    titulo: "Mosqueton 05",
-    imagen: "./productos/mosqueton05.jpg",
-    categoria: {
-      nombre: "Mosquetones",
-      id: "mosquetones",
-    },
-    precio: 5200,
-  },
-];
+// let productos = [
+//   {
+//     id: "arnes-01",
+//     titulo: "Arnes 01",
+//     imagen: "./productos/arnes01.jpg",
+//     categoria: {
+//       nombre: "Arneses",
+//       id: "arneses",
+//     },
+//     precio: 1500,
+//   },
+//   {
+//     id: "arnes-02",
+//     titulo: "Arnes 02",
+//     imagen: "./productos/arnes02.jpg",
+//     categoria: {
+//       nombre: "Arneses",
+//       id: "arneses",
+//     },
+//     precio: 1700,
+//   },
+//   {
+//     id: "arnes-03",
+//     titulo: "Arnes 03",
+//     imagen: "./productos/arnes03.jpg",
+//     categoria: {
+//       nombre: "Arneses",
+//       id: "arneses",
+//     },
+//     precio: 1200,
+//   },
+//   {
+//     id: "arnes-04",
+//     titulo: "Arnes 04",
+//     imagen: "./productos/arnes04.jpg",
+//     categoria: {
+//       nombre: "Arneses",
+//       id: "arneses",
+//     },
+//     precio: 1800,
+//   },
+//   {
+//     id: "casco-01",
+//     titulo: "Casco 01",
+//     imagen: "./productos/casco01.jpg",
+//     categoria: {
+//       nombre: "Cascos",
+//       id: "cascos",
+//     },
+//     precio: 10200,
+//   },
+//   {
+//     id: "casco-02",
+//     titulo: "Casco 02",
+//     imagen: "./productos/casco02.jpg",
+//     categoria: {
+//       nombre: "Cascos",
+//       id: "cascos",
+//     },
+//     precio: 10900,
+//   },
+//   {
+//     id: "casco-03",
+//     titulo: "Casco 03",
+//     imagen: "./productos/casco03.jpg",
+//     categoria: {
+//       nombre: "Cascos",
+//       id: "cascos",
+//     },
+//     precio: 15200,
+//   },
+//   {
+//     id: "casco-04",
+//     titulo: "Casco 04",
+//     imagen: "./productos/casco04.jpg",
+//     categoria: {
+//       nombre: "Cascos",
+//       id: "cascos",
+//     },
+//     precio: 17800,
+//   },
+//   {
+//     id: "casco-05",
+//     titulo: "Casco 05",
+//     imagen: "./productos/casco05.jpg",
+//     categoria: {
+//       nombre: "Cascos",
+//       id: "cascos",
+//     },
+//     precio: 19999,
+//   },
+//   {
+//     id: "cuerda-01",
+//     titulo: "Cuerda 01",
+//     imagen: "./productos/cuerda01.jpeg",
+//     categoria: {
+//       nombre: "Cuerdas",
+//       id: "cuerdas",
+//     },
+//     precio: 70200,
+//   },
+//   {
+//     id: "cuerda-02",
+//     titulo: "Cuerda 02",
+//     imagen: "./productos/cuerda02.jpg",
+//     categoria: {
+//       nombre: "Cuerdas",
+//       id: "cuerdas",
+//     },
+//     precio: 69200,
+//   },
+//   {
+//     id: "cuerda-03",
+//     titulo: "Cuerda 03",
+//     imagen: "./productos/cuerda03.jpg",
+//     categoria: {
+//       nombre: "Cuerdas",
+//       id: "cuerdas",
+//     },
+//     precio: 80000,
+//   },
+//   {
+//     id: "mosqueton-01",
+//     titulo: "Mosqueton 01",
+//     imagen: "./productos/mosqueton01.jpg",
+//     categoria: {
+//       nombre: "Mosquetones",
+//       id: "mosquetones",
+//     },
+//     precio: 3500,
+//   },
+//   {
+//     id: "mosqueton-02",
+//     titulo: "Mosqueton 02",
+//     imagen: "./productos/mosqueton02.jpg",
+//     categoria: {
+//       nombre: "Mosquetones",
+//       id: "mosquetones",
+//     },
+//     precio: 4500,
+//   },
+//   {
+//     id: "mosqueton-03",
+//     titulo: "Mosqueton 03",
+//     imagen: "./productos/mosqueton03.jpg",
+//     categoria: {
+//       nombre: "Mosquetones",
+//       id: "mosquetones",
+//     },
+//     precio: 4300,
+//   },
+//   {
+//     id: "mosqueton-04",
+//     titulo: "Mosqueton 04",
+//     imagen: "./productos/mosqueton04.jpg",
+//     categoria: {
+//       nombre: "Mosquetones",
+//       id: "mosquetones",
+//     },
+//     precio: 5150,
+//   },
+//   {
+//     id: "mosqueton-05",
+//     titulo: "Mosqueton 05",
+//     imagen: "./productos/mosqueton05.jpg",
+//     categoria: {
+//       nombre: "Mosquetones",
+//       id: "mosquetones",
+//     },
+//     precio: 5200,
+//   },
+// ];
+
+fetch("./productos.json")
+  .then(response => response.json())
+  .then(productos => {
+  
 // const contenedorProductos = document.querySelector("#contenedor-productos");
 const contenedorProductos = document.getElementById("contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
@@ -262,3 +267,4 @@ function agregarAlCarrito(e) {
     JSON.stringify(productosCarrito)
   );
 }
+})
